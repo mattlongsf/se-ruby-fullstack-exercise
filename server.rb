f80427d7a3472b5f1fb8dfe98efa4536d1f6d45c
Matt Long <matt.long@optimizely.com>
2018-05-31 13:57:53 -0700

    completed

--- server.rb
+++ server.rb
@@ -2,28 +2,30 @@ require 'sinatra'
 require 'httparty'
 require 'securerandom'
 require 'twilio-ruby'
+require "optimizely"
+
 
 # STEP 1: Add the Optimizely Full Stack Ruby gem
-# STEP 2: Require the Optimizely gem 
+# STEP 2: Require the Optimizely gem
 # STEP 3: Include the twilio account SID, auth token and phone number below
 
 # => Log into Twilio and access the account SID, token, and number
-TWILIO_NUMBER = '+14157022765'
-TWILIO_ACCOUNT_SID = 'replace with twilio sid'
-TWILIO_AUTH_TOKEN = 'replace with auth token'
+TWILIO_NUMBER = '+14159496288'
+TWILIO_ACCOUNT_SID = 'enter account id'
+TWILIO_AUTH_TOKEN = 'enter auth token'
 
 # Optimizely Setup
 
 # Step 4: Replace this url with your own Optimizely Project
 
-DATAFILE_URL = 'https://cdn.optimizely.com/public/8785893177/s/10677861955_10677861955.json'
+DATAFILE_URL = 'https://cdn.optimizely.com/public/8490221086/s/10795020673_10795020673.json'
 
 DATAFILE_URI_ENCODED = URI(DATAFILE_URL)
 
-# => Step 5: Use a library, such as HTTParty, to get grab the datafile from the CDN 
+# => Step 5: Use a library, such as HTTParty, to get grab the datafile from the CDN
 #         https://github.com/jnunemaker/httparty#examples
 #         example: response = HTTParty.get('http://api.stackexchange.com/2.2/questions?site=stackoverflow').body
-#         The above line will return the body of the http request 
+#         The above line will return the body of the http request
 #         NOTE: use the uri encoded url shown above :)
 
 # => Step 6: Initialize the Optimizely SDK using the json retrieved from step 4
@@ -39,12 +41,12 @@ get '/' do
 end
 
 # => GET endpoint to receive messages, this should be setup as a webhook in Twilio
-# => anytime twilio receives a message on our number, Twilio will make a request to this endpoint 
+# => anytime twilio receives a message on our number, Twilio will make a request to this endpoint
 get '/sms' do
-  
+
   # => Getting the number that texted the sms service
 	sender_number = params[:From]
-  
+
   # => Getting the message that was sent to the service
   # => We could use this to understand what the user said, and create a conversational dialog
 	text_body = params[:Body]
@@ -62,6 +64,17 @@ get '/sms' do
 	# => Example, test out different messages in your response
 	# => Using the helper function to reply to the number who messaged the sms service
   # => example: send_sms "Hey this is a response!" sender_number
+  datafile = HTTParty.get(DATAFILE_URL).body
+
+  optimizely_client = Optimizely::Project.new(datafile)
+
+  enabled = optimizely_client.is_feature_enabled('sms_response', user_id)
+
+  if enabled
+    text = optimizely_client.get_feature_variable_string('sms_response', 'text', user_id)
+    send_sms text, sender_number
+  end
+
 
 end
 
@@ -75,6 +88,6 @@ def send_sms body, number
       from: TWILIO_NUMBER,
       to: number,
       body: body
-    ) 
+    )
 end
 
